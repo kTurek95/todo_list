2e185d4d15020d8cd52c21d25ae90176b9f3d95d
kTurek95 <k.turek1995@gmail.com>
2023-06-30 10:19:04 +0200

    Adding several tests

--- tests.py
+++ tests.py
@@ -1,4 +1,5 @@
 import unittest
+import function as fn
 
 
 class TestAddTask(unittest.TestCase):
@@ -18,6 +19,42 @@ class TestAddTask(unittest.TestCase):
 
     def test_empty_task(self):
         pass
+
+
+class TestDisplayTask(unittest.TestCase):
+    def test_one_task(self):
+        user_tasks = []
+        user_task = 'wash the car'
+        user_tasks.append(user_task)
+        self.assertEqual(user_tasks, ['wash the car'])
+
+    def test_few_tasks(self):
+        user_tasks = ['take out the trash', 'wash the car']
+        expected_output = "Your tasks are:\ntake out the trash\nwash the car"
+        output = fn.display_tasks(user_tasks)
+        self.assertEqual(output, expected_output)
+
+    def test_empty_task(self):
+        user_tasks = []
+        expected_output = "You don't have any tasks on the list."
+        output = fn.display_tasks(user_tasks)
+        self.assertEqual(expected_output, output)
+
+
+class TestDeleteTask(unittest.TestCase):
+    pass
+
+
+class TestMarkTaskCompleted(unittest.TestCase):
+    pass
+
+
+class TestSaveTaskToFile(unittest.TestCase):
+    pass
+
+
+class TestLoadTaskFromFile(unittest.TestCase):
+    pass
 
 
 def main():
