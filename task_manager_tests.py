14f2d5b1f465bbc0e2696c1180f7937781c4e950
kTurek95 <k.turek1995@gmail.com>
2023-10-14 14:13:17 +0100

    Change module name from tests to taks_manager_tests.py and updating the testing approach due to the transition to object-oriented code of the module being tested

--- task_manager_tests.py
+++ task_manager_tests.py
@@ -13,7 +13,7 @@ Classes:
 import unittest
 import io
 from unittest.mock import mock_open, patch
-import function as fn
+from task_manager import TaskManager
 
 
 class TestAddTask(unittest.TestCase):
@@ -25,6 +25,7 @@ class TestAddTask(unittest.TestCase):
         Set up a clean user_tasks list before each test.
         """
         self.user_tasks = []
+        self.task_manager = TaskManager()
 
     def test_add_task(self):
         """
@@ -33,8 +34,8 @@ class TestAddTask(unittest.TestCase):
         It checks if the function correctly adds a task to the user_tasks list.
         """
         with patch('builtins.input', return_value='Test Task'):
-            fn.add_task(self.user_tasks)
-        self.assertIn('Test Task', self.user_tasks)
+            self.task_manager.add_task()
+        self.assertIn('Test Task', self.task_manager.user_tasks)
 
     def test_display_tasks_with_tasks(self):
         """
@@ -42,10 +43,19 @@ class TestAddTask(unittest.TestCase):
 
         It verifies if the function correctly formats and returns the list of tasks.
         """
-        tasks = ['Task 1', 'Task 2', 'Task 3']
-        result = fn.display_tasks(tasks)
-        expected = "Your tasks are:\nTask 1\nTask 2\nTask 3"
-        self.assertEqual(result, expected)
+
+        with patch('builtins.input', return_value='Test Task'):
+            self.task_manager.add_task()
+
+        with patch('sys.stdout', new_callable=io.StringIO) as mock_stdout:
+            self.task_manager.display_tasks()
+
+        output = mock_stdout.getvalue().strip()
+
+        expected = 'Your tasks are:\n1. Test Task'
+
+        self.assertEqual(output, expected)
+        self.assertNotIn('You don\'t have any tasks on the list.\n', output)
 
     def test_display_tasks_without_tasks(self):
         """
@@ -53,10 +63,12 @@ class TestAddTask(unittest.TestCase):
 
         It checks if the function returns the correct message when there are no tasks.
         """
-        tasks = []
-        result = fn.display_tasks(tasks)
-        expected = "You don't have any tasks on the list."
-        self.assertEqual(result, expected)
+        with patch('sys.stdout', new_callable=io.StringIO) as mock_stdout:
+            self.task_manager.display_tasks()
+
+        output = mock_stdout.getvalue()
+        expected = "You don't have any tasks on the list.\n"
+        self.assertEqual(output, expected)
 
     def test_delete_task(self):
         """
@@ -64,10 +76,10 @@ class TestAddTask(unittest.TestCase):
 
         It verifies if the function correctly removes a task from the user_tasks list.
         """
-        user_tasks = ['take out the trash', 'wash the car']
-        task_to_delete = 'wash the car'
-        fn.delete_task(task_to_delete, user_tasks)
-        self.assertNotIn(task_to_delete, user_tasks)
+        self.user_tasks = ['take out the trash', 'wash the car']
+        with patch('builtins.input', return_value='wash the car'):
+            self.task_manager.delete_task()
+        self.assertNotIn('wash the car', self.task_manager.user_tasks)
 
     def test_delete_task_empty_list(self):
         """
@@ -76,12 +88,10 @@ class TestAddTask(unittest.TestCase):
         It checks if the function handles the case when the list is empty
         and returns the correct message.
         """
-
-        user_tasks = []
-        task_to_delete = 'wash the car'
-        with unittest.mock.patch('sys.stdout', new_callable=io.StringIO) as mock_stdout:
-            fn.delete_task(task_to_delete, user_tasks)
-            self.assertEqual(mock_stdout.getvalue(), 'Task does not exist\n')
+        with patch('builtins.input', return_value='wash the car'):
+            with unittest.mock.patch('sys.stdout', new_callable=io.StringIO) as mock_stdout:
+                self.task_manager.delete_task()
+                self.assertEqual(mock_stdout.getvalue(), 'Task does not exist\n')
 
     def test_mark_completed_task(self):
         """
@@ -90,22 +100,24 @@ class TestAddTask(unittest.TestCase):
         It checks if the function correctly moves a task to the completed_user_tasks list.
         """
         user_tasks = []
-        fn.user_tasks.append('Test Task')
+        self.task_manager.user_tasks.append('Test Task')
         with patch('builtins.input', return_value='Test Task'):
-            fn.mark_completed_task()
-        self.assertIn('Test Task', fn.completed_user_tasks)
+            self.task_manager.mark_completed_task()
+        self.assertIn('Test Task', self.task_manager.completed_user_tasks)
         self.assertNotIn('Test Task', user_tasks)
 
-    @patch('builtins.input', return_value='Test Task')
     @patch('builtins.open', new_callable=mock_open)
-    def test_save_task_to_file(self, mock_file_open, mock_input):
+    def test_save_task_to_file(self, mock_file_open):
         """
         Test the save_task_to_file function.
 
         It checks if the function correctly saves a task to a file.
         """
-        tasks = ['Test Task']
-        fn.save_task_to_file(tasks)
+        with patch('builtins.input', return_value='Test Task'):
+            self.task_manager.add_task()
+
+        with patch('builtins.input', return_value='Test Task'):
+            self.task_manager.save_task_to_file()
         mock_file_open.assert_called_once_with('tasks.txt', 'a', encoding='utf8')
         mock_file_open().write.assert_called_once_with('Test Task\n')
 
@@ -117,7 +129,7 @@ class TestAddTask(unittest.TestCase):
         It verifies if the function correctly loads tasks from a file and displays them.
         """
         with patch('builtins.print') as mock_print:
-            fn.load_tasks_from_file()
+            self.task_manager.load_tasks_from_file()
 
         mock_file_open.assert_called_once_with('tasks.txt', 'r', encoding='utf8')
         mock_print.assert_has_calls([
